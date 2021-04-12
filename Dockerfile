fe1e51f313349509013455b433dbb75035123c0d
jwons <joe.wonsil@gmail.com>
2021-04-12 11:22:39 -0700

    prepare dataset for final commit to Docker image, adjusted figure font sizes

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM jupyter/scipy-notebook:4d9c9bd9ced0
+
+COPY . /home/jovyan/work
+
--- start_container.sh
+++ start_container.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+#docker build . -t raas-eval
+#
+#docker run \
+#  --rm \
+#  -e JUPYTER_ENABLE_LAB=yes \
+#  -p 8888:8888 \
+#  raas-eval
+
 docker run \
+  --rm \
   -e JUPYTER_ENABLE_LAB=yes \
   -p 8888:8888 \
   --name raas-eval \
   --mount type=bind,source="$(pwd)",target=/home/jovyan/work \
-  jupyter/scipy-notebook
-
+  jupyter/scipy-notebook:4d9c9bd9ced0
