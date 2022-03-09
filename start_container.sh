87a71fe2410818dc2bd8f7a55a60319fbe48569d
jwons <joe.wonsil@gmail.com>
2022-03-09 15:07:41 -0800

    changed startup script so it would load data into container rather than mount working directory to allow for experimentation without messing with repo

--- start_container.sh
+++ start_container.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-#docker build . -t raas-eval
+docker build . -t raas-eval
 #
-#docker run \
-#  --rm \
-#  -e JUPYTER_ENABLE_LAB=yes \
-#  -p 8888:8888 \
-#  raas-eval
-
 docker run \
   --rm \
   -e JUPYTER_ENABLE_LAB=yes \
   -p 8888:8888 \
-  --name raas-eval \
-  --mount type=bind,source="$(pwd)",target=/home/jovyan/work \
-  jupyter/scipy-notebook:4d9c9bd9ced0
+  raas-eval
+
+#docker run \
+#  --rm \
+#  -e JUPYTER_ENABLE_LAB=yes \
+#  -p 8888:8888 \
+#  --name raas-eval \
+#  --mount type=bind,source="$(pwd)",target=/home/jovyan/work \
+#  jupyter/scipy-notebook:4d9c9bd9ced0
