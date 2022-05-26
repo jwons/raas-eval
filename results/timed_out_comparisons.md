b72858ee0a67558686f68a9df6c9e4ba331ba4d2
jwons <joe.wonsil@gmail.com>
2022-05-25 17:32:41 -0700

    data changes, working on timeout table

---
+++ results/timed_out_comparisons.md
@@ -0,0 +1,17 @@
+
+-------------------------------------------------------------------------
+    Datasets                                                      Scripts
+  ---------- ------------- ------------ ---------------- ----------------
+       Total   TO w/o RaaS   TO w/ RaaS   Both Completed   Both Completed
+
+        3889             2           16             2484             SC_BOTH_DONE_TOTAL
+
+        100%      0.05143%      0.4114%           63.87%          SC_BOTH_DONE_PERC%\*
+
+-------------------------------------------------------------------------
+
+Table: This table displays the number of datasets that time out (TO) during
+the evaluations, and the number that completed with and without RaaS.
+\*Note that for the number of scripts that completed in both, the
+percentage is out of the total number of scripts: 11819. The rest of
+the percentages are out of the total number of datasets: 2900. {#tbl:timeout-results}
--- scripts/get_dbs.py
+++ scripts/get_dbs.py
@@ -1,15 +1,55 @@
 import os
+import argparse
 
-ip_list = os.environ.get("IP_LIST").split(";")
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--all', action='store_true')
+parser.add_argument('--dbs', action='store_true')
+parser.add_argument('--touts', action='store_true')
+parser.add_argument('--dirs', action='store_true')
+parser.add_argument('--vms', nargs='+')
+
+args = parser.parse_args()
+
+
+vms = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+if(args.vms):
+    vms = args.vms
+
+dbs = False
+touts = False
+dirs = False
+
+if args.dbs: dbs = True 
+if args.touts: touts = True 
+if args.dirs: dirs = True
+
+if args.all:
+    dbs = True 
+    touts = True 
+    dirs = True
+
+
+
+ip_temp_list = os.environ.get("IP_LIST").split(";")
+ip_list = []
+for vm in vms:
+    ip_list.append(ip_temp_list[int(vm)])
+
+print(ip_list)
 
 copy_db_command = "scp -i ~/.ssh/id_rsa ubuntu@HOST:/home/ubuntu/raas/db/app.db ../data/raas_dbs/NUMBER-app.db"
 copy_timeouts_command = "scp -i ~/.ssh/id_rsa ubuntu@HOST:/home/ubuntu/raas/eval/raas_timeout_dois.txt ../data/raas_timeouts/NUMBER-timeout-dois.txt"
+copy_prov_dirs_command = "scp -i ~/.ssh/id_rsa ubuntu@HOST:/home/ubuntu/raas/eval/prov_dirs.tar ../data/prov_dirs/NUMBER-prov_dirs.tar"
 
 counter = 0
 for ip_addr in ip_list:
-    ip_db_command = copy_db_command.replace("HOST", ip_addr).replace("NUMBER", str(counter))
-    ip_timeout_command = copy_timeouts_command.replace("HOST", ip_addr).replace("NUMBER", str(counter))
+    ip_db_command = copy_db_command.replace("HOST", ip_addr).replace("NUMBER", str(vms[counter]))
+    ip_timeout_command = copy_timeouts_command.replace("HOST", ip_addr).replace("NUMBER", str(vms[counter]))
+    ip_dirs_command = copy_prov_dirs_command.replace("HOST", ip_addr).replace("NUMBER", str(vms[counter]))
 
     counter += 1
-    os.system(ip_db_command)
-    os.system(ip_timeout_command)
+    if dbs: os.system(ip_db_command)
+    if touts: os.system(ip_timeout_command)
+    if dirs: os.system(ip_dirs_command)
